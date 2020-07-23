8e032c9ba5cabfb1478b2266fe3111572117b631
Elijahscriptdev <elijahobominuru@gmail.com>
2020-07-23 16:04:03 +0100

    add navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,8 +10,16 @@ const Navbar = (props) => {
   return(
     <div className="navbar">
         <ul className="navbarMenu">
-            <li></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
         </ul>
+        <div className="search">
+          <input type="text" placeholder="search" />
+          <img alt="search" src={require('../../assets/icons/search.png')} />
+        </div>
     </div>
    )
 
