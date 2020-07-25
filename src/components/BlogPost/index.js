086bc8b73cb10e28d97c8d2c28ea5e067ec3f535
Elijahscriptdev <elijahobominuru@gmail.com>
2020-07-25 17:01:07 +0100

    add blog post header

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -9,9 +9,15 @@ import Card from '../UI/Card';
 
 const BlogPost = (props) => {
   return(
-    <Card style={{width:'70%'}}>
-        My first Blog Post
+    <div className="blogPostContainer">
+    <Card>
+      <div className="blogHeader">
+        <span className="blogCategory">Featured</span>
+        <h1 className="postTitle">Beautiful is always beautiful</h1>
+        <span className="postedBy">posted on july 25, 2020 by Elijah-dev</span>
+      </div>
     </Card>
+    </div>
    )
 
  }
