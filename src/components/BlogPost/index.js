4021c65556561039ebdf936144da17f0d7cab17c
Elijahscriptdev <elijahobominuru@gmail.com>
2020-07-25 19:51:26 +0100

    add images

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -16,6 +16,10 @@ const BlogPost = (props) => {
         <h1 className="postTitle">Beautiful is always beautiful</h1>
         <span className="postedBy">posted on july 25, 2020 by Elijah-dev</span>
       </div>
+
+      <div className="postImageContainer">
+        <img alt="Elijah-dev" src="https://res.cloudinary.com/elijjaaahhhh/image/upload/c_limit,h_1518,w_2901/v1584497691/austin-distel-uPK2TbJlvMQ-unsplash_pfgwwg.jpg" />
+      </div>
     </Card>
     </div>
    )
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -9,9 +9,20 @@ import './style.css';
 
 const Sidebar = (props) => {
   return(
-    <Card style={{width:'27%'}}>
-        Sidebar
-    </Card>
+    <div className="sideBarContainer">
+      <Card style={{ marginBottom:"20px" }}>
+        <div className="sideHeader">
+          <span className="sideTitle">About-Us</span>
+          <img alt="Elijah" src="http://res.cloudinary.com/elijjaaahhhh/image/upload/c_scale,w_408/v1595702797/bosses_waypez.jpg" />
+        </div>
+      </Card>
+      <Card>
+        <div className="sideHeader network">
+          <span className="sideTitle">Social Networks</span>
+          <img alt="Elijah" src={require("../../containers/blogPostImages/nysc1.jpg")} />
+        </div>
+      </Card>
+    </div>
    )
 
  }
