8a8fed271d36867ff684ea9c87aeb393f348b497
Elijahscriptdev <elijahobominuru@gmail.com>
2020-07-30 21:27:20 +0100

    refactor code

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import Card from '../../components/UI/Card';
 import Sidebar from '../../components/Sidebar';
+import RecentPost from './RecentPost';
 // import Hero from '../../components/Hero';
 
 
@@ -31,30 +32,7 @@ const Home = props => {
             </Card>
 
             <section className="HomeContainer">
-                <div style={{width:"70%"}}>
-                    <Card style={{marginBottom:"20px"}}>
-                        <div className="postImageWrapper">
-                            <img src="https://res.cloudinary.com/elijjaaahhhh/image/upload/v1584497692/hermes-rivera-qbf59TU077Q-unsplash_mc7lec.jpg"/>
-                        </div>
-
-                        <div style={{textAlign:"center"}}>
-                            <span>Featured</span>
-                            <h2>Fitness Mantra To Live Fit Life</h2>
-                            <span>
-                            Midst first it, you're multiply divided. There don't, 
-                            second his one given the he one third rule fruit, very. Fill. S
-                            eed give firmament doesn't land, isn't lesser creeping. Abundantly you 
-                            called signs waters yielding he cattle greater were evening. Sixth make 
-                            moving the multiply dominion creature beast made subdue lights him. 
-                            Green of lights in their first. It there winged called after upon him.
-                            </span>
-                        </div>
-                    </Card>
-
-                    <Card style={{marginBottom:"20px"}}>
-                        Post 2
-                    </Card>
-                </div>
+                <RecentPost />
 
                 <Sidebar />
 
